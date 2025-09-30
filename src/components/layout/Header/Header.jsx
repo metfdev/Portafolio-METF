bf1c640af3751ddf118b9561158364f78c61c0be
Miguel Ticaray <142277133+metfdev@users.noreply.github.com>
2025-09-30 16:33:39 -0400

    Refactor layout and styling in Header and Home
    Improved Header component layout for better responsiveness and alignment, updating flex/grid usage and class names. Adjusted Home page structure by moving ParticleBg above main content and modifying container height and dark mode background classes for enhanced visual consistency.

--- src/components/layout/Header/Header.jsx
+++ src/components/layout/Header/Header.jsx
@@ -32,21 +32,22 @@ function Header(){
 
   return (
     <header
-        className="grid lg:grid-cols-2 gap-8 justify-items-center content-center text-left cursor-default 2xl:gap-5 transition-all ease-in duration-200">
-        <div className="flex flex-col items-start gap-8  ">
+        className="flex flex-col items-center justify-center lg:grid lg:grid-cols-2 lg:justify-items-center lg:content-center gap-12 lg:gap-5 xl:gap-12 px-8 xl:px-0 cursor-default lg:w-[1100px] xl:w-[1400px] transition-all ease-in duration-200">
+        <div className="flex flex-col items-start  justify-center gap-8">
           <div className="bg-zinc-800/80 py-1 px-2 rounded-full hover:bg-zinc-800/60 transition-all ease-in duration-200">
             <h3 className="text-sm text-center text-gray-400 font-semibold ">Disponible para oportunidades</h3>
           </div>
-          <div className="flex flex-col gap-5">
-            <h1 className="text-5xl lg:text-6xl font-bold text-gray-100 flex flex-col gap-4 ">Hola, soy
+          <div className="flex flex-col gap-5 justify-center items-start  ">
+            <h1 className="text-5xl lg:text-6xl font-bold text-gray-100 flex flex-col gap-4 ">
+              Hola, soy
               <span className="bg-gradient-to-r from-cyan-500 to-teal-400 bg-clip-text text-transparent pb-2 font-inter ">
                 Miguel Ticaray
               </span>
             </h1>
-            <div className="flex flex-col gap-8">
-              <div className="flex flex-col gap-5 pr-3">
+            <div className="flex flex-col items-start  justify-center gap-8">
+              <div className="flex flex-col justify-center items-start gap-5">
                 <p className="text-gray-400 text-lg lg:text-2xl font-semibold ">Desarrollador Frontend Junior</p>
-                <p className="text-gray-400 text-md lg:text-xl font-semibold text-balance text-left ">
+                <p className="text-gray-400 text-md lg:text-xl font-semibold lg:text-left ">
                   Apasionado por crear experiencias web hermosas y funcionales con tecnologías modernas y diseño
                   responsivo. Siempre ansioso por aprender y crecer en el mundo en constante evolución del desarrollo web.
                 </p>
@@ -63,7 +64,7 @@ function Header(){
               </div>
             </div>
           </div>
-          <div className="flex gap-3 items-center justify-start">
+          <div className="flex gap-3">
             <button
               className="flex gap-3 items-center px-8 py-3 bg-gradient-to-r from-cyan-500 to-teal-400 rounded-xl border border-cyan-500 text-black font-semibold text-sm shadow-2xl shadow-cyan-800/20 brightness-110 hover:transform hover:translate-y-[-2px] transition-all ease-in duration-200 xl:cursor-pointer group/contact ">
               <img className="w-4 transition-all ease-in duration-200 group-hover/contact:scale-110"
@@ -71,13 +72,13 @@ function Header(){
               Contactar
             </button>
             <button
-              className="flex gap-3 items-center px-8 py-3 bg-gradient-to-r from-black to-gray-900 rounded-xl border border-gray-800 text-white  font-semibold text-sm shadow-2xl shadow-cyan-900/20 hover:brightness-150 hover:border-cyan-400 transition-all ease-in duration-200 xl:cursor-pointer group/download">
+              className="flex gap-3 items-center px-8 py-3 bg-gradient-to-r from-black to-gray-900 rounded-xl border border-gray-800 text-white font-semibold text-sm shadow-2xl shadow-cyan-900/20 hover:brightness-150 hover:border-cyan-400 transition-all ease-in duration-200 xl:cursor-pointer group/download">
               <img className="w-4 transition-all ease-in duration-200 group-hover/download:scale-110"
                 src="./img/icons/download-white.svg" alt="download"/>
               Descargar CV
             </button>
           </div>
-          <div className="flex  gap-4 justify-">
+          <div className="flex gap-4">
             <a href="https://github.com/METF98" target="_blank"
               className="p-3 bg-teal-500/30 lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/github  ">
               <img className=" w-4  lg:group-hover/github:brightness-0 " src="./img/icons/github-white.svg"
@@ -95,7 +96,7 @@ function Header(){
             </a>
           </div>
         </div>
-        <div className="flex justify-center 2xl:justify-end items-center  relative">
+        <div className="flex justify-center  items-center lg:w-full relative">
           <div ref={openToWork}
             className="absolute -top-2 lg:top-20 -right-5   z-10 text-[12px] text-zinc-900 font-semibold rounded-xl px-3 py-1 bg-gradient-to-r from-cyan-500 form-80% to-teal-400 lg:hover:brightness-110  ">
             <p>Open to Work</p>
