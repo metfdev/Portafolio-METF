0fd7182c3a8a526f957658cd40d559534fc06c1d
Miguel Ticaray <142277133+metfdev@users.noreply.github.com>
2025-11-03 17:04:57 -0400

    Agregar animación de entrada en la sección de inicio y mejorar la estructura del componente Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useContext } from "react";
+import { useContext, useRef, useEffect,useState } from "react";
+import { gsap } from "gsap";
 import {
   BrowserRouter as Router,
   Routes,
@@ -13,20 +14,35 @@ import { Contact } from "./pages/Contact";
 import { NoFound } from "./pages/NoFound";
 import { ThemeContext } from "./context/ThemeProvider";
 
-
 function App() {
   const { tema } = useContext(ThemeContext);
+  const homeSection = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      homeSection.current,
+      {
+        width: "100%",
+      },
+      {
+        width: 0,
+        duration: 1,
+        ease: "power3.inOut",
+      }
+    );
+  }, [tema]);
 
   return (
     <Router>
-      <section className={tema === "dark" ? "dark" : ""}>
-        <div className="min-h-screen flex flex-col gap-10 bg-radial from-slate-300 to-neutral-100  text-gray-900  [&::-webkit-scrollbar]:w-2 lg:[&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:bg-white [::-webkit-scrollbar-thumb]:bg-white [&::-webkit-scrollbar-thumb]:bg-cyan-800 [&::-webkit-scrollbar-thumb]:rounded-lg hover:[&::-webkit-scrollbar-thumb]:bg-teal-800 dark:from-slate-950  dark:to-neutral-950 dark:text-gray-100 dark:[&::-webkit-scrollbar-track]:bg-black dark:[::-webkit-scrollbar-thumb]:bg-black dark:[&::-webkit-scrollbar-thumb]:bg-cyan-500 dark:hover:[&::-webkit-scrollbar-thumb]:bg-teal-500">
-          <NavBar />
-          <main className="flex-1 flex items-center h-full justify-center overflow-hidden ">
-            <AppRoutes />
-          </main>
-          <Footer />
-        </div>
+      <section className={tema === "dark" ? "dark relative" : "relative"}>
+        <div ref={homeSection}  className="bg-neutral-900 absolute top-0 left-0 right-0 bottom-0 z-[999999]"></div>
+          <div className="min-h-screen flex flex-col gap-10 bg-radial from-slate-300 to-neutral-100  text-gray-900  [&::-webkit-scrollbar]:w-2 lg:[&::-webkit-scrollbar]:w-3 [&::-webkit-scrollbar-track]:bg-white [::-webkit-scrollbar-thumb]:bg-white [&::-webkit-scrollbar-thumb]:bg-cyan-800 [&::-webkit-scrollbar-thumb]:rounded-lg hover:[&::-webkit-scrollbar-thumb]:bg-teal-800 dark:from-slate-950  dark:to-neutral-950 dark:text-gray-100 dark:[&::-webkit-scrollbar-track]:bg-black dark:[::-webkit-scrollbar-thumb]:bg-black dark:[&::-webkit-scrollbar-thumb]:bg-cyan-500 dark:hover:[&::-webkit-scrollbar-thumb]:bg-teal-500">
+            <NavBar />
+            <main className="flex-1 flex items-center h-full justify-center overflow-hidden ">
+              <AppRoutes />
+            </main>
+            <Footer />
+          </div>
       </section>
     </Router>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -37,128 +37,131 @@ function Home() {
     );
   }, []);
 
+
   return (
-    <section className="page flex flex-col items-center justify-center lg:grid lg:grid-cols-2 lg:justify-items-center lg:content-center gap-20 lg:gap-5 pb-10 px-8 xl:px-0 cursor-default lg:w-[1100px] xl:w-[1200px] 2xl:w-[1300px] transition-all ease-in duration-200">
-      <section className="flex flex-col items-start justify-center gap-8">
-        <div className="bg-zinc-800/80 py-1 px-2 rounded-full hover:bg-zinc-800/60 transition-all ease-in duration-200">
-          <h3 className="text-sm text-center  text-gray-100 font-semibold ">
-            Available for opportunities
-          </h3>
-        </div>
-        <div className="flex flex-col gap-5 justify-center items-start">
-          <h1 className="text-5xl lg:text-6xl bg-gradient-to-r font-bold from-10% from-cyan-800 to-teal-600  dark:from-cyan-500 dark:to-teal-400  bg-clip-text text-transparent pb-4 font-inter">
-            Miguel Ticaray
-          </h1>
-          <div className="flex flex-col items-start  justify-center gap-8">
-            <div className="flex flex-col justify-center items-start gap-5">
-              <p className=" text-lg lg:text-2xl font-semibold ">
-                Junior Web Developer
-              </p>
-              <p className=" text-md lg:text-xl font-semibold lg:text-left">
-                Passionate about creating beautiful, functional web experiences
-                with modern technologies. I specialize in React, JavaScript, and
-                responsive design, always eager to learn and grow in the
-                ever-evolving world of web development.
-              </p>
-            </div>
-            <div className="flex flex-col md:flex-row gap-4">
-              <div className="flex gap-2 items-center">
-                <Img
-                  style="w-4 brightness-0 dark:brightness-100 "
-                  src="./img/icons/mail-white.svg"
-                  alt="mail"
-                />
-                <p className=" font-semibold text-sm">
-                  Miguelticaray@gmail.com
+    <div>
+      <section className="page flex flex-col items-center justify-center lg:grid lg:grid-cols-2 lg:justify-items-center lg:content-center gap-20 lg:gap-5 pb-10 px-8 xl:px-0 cursor-default lg:w-[1100px] xl:w-[1200px] 2xl:w-[1300px] transition-all ease-in duration-200">
+        <section className="flex flex-col items-start justify-center gap-8">
+          <div className="bg-zinc-800/80 py-1 px-2 rounded-full hover:bg-zinc-800/60 transition-all ease-in duration-200">
+            <h3 className="text-sm text-center  text-gray-100 font-semibold ">
+              Available for opportunities
+            </h3>
+          </div>
+          <div className="flex flex-col gap-5 justify-center items-start">
+            <h1 className="text-5xl lg:text-6xl bg-gradient-to-r font-bold from-10% from-cyan-800 to-teal-600  dark:from-cyan-500 dark:to-teal-400  bg-clip-text text-transparent pb-4 font-inter">
+              Miguel Ticaray
+            </h1>
+            <div className="flex flex-col items-start  justify-center gap-8">
+              <div className="flex flex-col justify-center items-start gap-5">
+                <p className=" text-lg lg:text-2xl font-semibold ">
+                  Junior Web Developer
+                </p>
+                <p className=" text-md lg:text-xl font-semibold lg:text-left">
+                  Passionate about creating beautiful, functional web experiences
+                  with modern technologies. I specialize in React, JavaScript, and
+                  responsive design, always eager to learn and grow in the
+                  ever-evolving world of web development.
                 </p>
               </div>
-              <div className="flex gap-2 items-center ">
-                <Img
-                  style="w-4 brightness-0 dark:brightness-100"
-                  src="./img/icons/pingLocation.svg"
-                  alt="location"
-                />
-                <p className="font-semibold text-sm">Anaco, Venezuela</p>
+              <div className="flex flex-col md:flex-row gap-4">
+                <div className="flex gap-2 items-center">
+                  <Img
+                    style="w-4 brightness-0 dark:brightness-100 "
+                    src="./img/icons/mail-white.svg"
+                    alt="mail"
+                  />
+                  <p className=" font-semibold text-sm">
+                    Miguelticaray@gmail.com
+                  </p>
+                </div>
+                <div className="flex gap-2 items-center ">
+                  <Img
+                    style="w-4 brightness-0 dark:brightness-100"
+                    src="./img/icons/pingLocation.svg"
+                    alt="location"
+                  />
+                  <p className="font-semibold text-sm">Anaco, Venezuela</p>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex items-center justify-center gap-3">
-          <Button style="flex gap-3 items-center px-8 py-3 bg-gradient-to-r from-10% from-cyan-800 to-teal-600 dark:from-cyan-500 dark:to-teal-400 rounded-xl border dark:border-cyan-500 border-cyan-600 text-white dark:text-black font-semibold text-sm shadow-2xl shadow-cyan-800/20 brightness-110 hover:transform hover:translate-y-[-2px] transition-all ease-in duration-200 xl:cursor-pointer group/contact ">
-            <Img
-              style="w-4 transition-all dark:brightness-0 brightness-100 ease-in duration-200 group-hover/contact:scale-110"
-              src="./img/icons/mail-white.svg"
-              alt="mail"
-            />
-            Get in touch
-          </Button>
-          <Button style="flex gap-3 items-center px-8 py-3 dark:bg-gradient-to-r dark:from-black dark:to-gray-900 bg-cyan-500/10 rounded-xl border border-gray-400 hover:border-cyan-600 dark:border-gray-800 dark:text-white font-semibold text-sm shadow-2xl shadow-cyan-900/20 dark:hover:brightness-150 dark:hover:border-cyan-400 transition-all ease-in duration-200 xl:cursor-pointer group/download">
-            <Img
-              style="w-4 brightness-0 dark:brightness-100 transition-all ease-in duration-200 group-hover/download:scale-110"
-              src="./img/icons/download-white.svg"
-              alt="download"
-            />
-            View CV
-          </Button>
-        </div>
-        <div className="flex gap-4">
-          <a
-            href="https://github.com/METFDEV"
-            target="_blank"
-            className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/github  "
+          <div className="flex items-center justify-center gap-3">
+            <Button style="flex gap-3 items-center px-8 py-3 bg-gradient-to-r from-10% from-cyan-800 to-teal-600 dark:from-cyan-500 dark:to-teal-400 rounded-xl border dark:border-cyan-500 border-cyan-600 text-white dark:text-black font-semibold text-sm shadow-2xl shadow-cyan-800/20 brightness-110 hover:transform hover:translate-y-[-2px] transition-all ease-in duration-200 xl:cursor-pointer group/contact ">
+              <Img
+                style="w-4 transition-all dark:brightness-0 brightness-100 ease-in duration-200 group-hover/contact:scale-110"
+                src="./img/icons/mail-white.svg"
+                alt="mail"
+              />
+              Get in touch
+            </Button>
+            <Button style="flex gap-3 items-center px-8 py-3 dark:bg-gradient-to-r dark:from-black dark:to-gray-900 bg-cyan-500/10 rounded-xl border border-gray-400 hover:border-cyan-600 dark:border-gray-800 dark:text-white font-semibold text-sm shadow-2xl shadow-cyan-900/20 dark:hover:brightness-150 dark:hover:border-cyan-400 transition-all ease-in duration-200 xl:cursor-pointer group/download">
+              <Img
+                style="w-4 brightness-0 dark:brightness-100 transition-all ease-in duration-200 group-hover/download:scale-110"
+                src="./img/icons/download-white.svg"
+                alt="download"
+              />
+              View CV
+            </Button>
+          </div>
+          <div className="flex gap-4">
+            <a
+              href="https://github.com/METFDEV"
+              target="_blank"
+              className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/github  "
+            >
+              <img
+                className=" w-4 brightness-0 dark:brightness-100 dark:lg:group-hover/github:brightness-0 lg:group-hover/github:brightness-100 "
+                src="./img/icons/github-white.svg"
+                alt="github icono"
+              />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/miguel-ticaray-836bb6222/"
+              target="_blank"
+              className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/linkedin"
+            >
+              <img
+                className=" w-4 brightness-0 dark:brightness-100 dark:lg:group-hover/linkedin:brightness-0 lg:group-hover/linkedin:brightness-100 "
+                src="./img/icons/linkedin-white.svg"
+                alt="linkedin icono"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/miguelticaray/"
+              target="_blank"
+              className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/instagram"
+            >
+              <img
+                className=" w-4 brightness-0 dark:brightness-100  dark:lg:group-hover/instagram:brightness-0 lg:group-hover/instagram:brightness-100 "
+                src="./img/icons/instagram-white.svg"
+                alt="instagram icono"
+              />
+            </a>
+          </div>
+        </section>
+        <section className="flex justify-center 2xl:justify-end  items-center lg:w-full relative transition-all ease-in duration-200">
+          <div
+            ref={openToWork}
+            className="absolute -top-2 lg:top-5 -right-5 lg:right-2 xl:right-10 2xl:-right-15 z-10 text-[12px] text-zinc-900 font-semibold rounded-xl px-3 py-1 bg-gradient-to-r from-cyan-500 form-80% to-teal-400 lg:hover:brightness-110  "
           >
-            <img
-              className=" w-4 brightness-0 dark:brightness-100 dark:lg:group-hover/github:brightness-0 lg:group-hover/github:brightness-100 "
-              src="./img/icons/github-white.svg"
-              alt="github icono"
-            />
-          </a>
-          <a
-            href="https://www.linkedin.com/in/miguel-ticaray-836bb6222/"
-            target="_blank"
-            className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/linkedin"
+            <p>Open to Work</p>
+          </div>
+          <div
+            ref={experience}
+            className="absolute -bottom-5 lg:bottom-2  -left-5 lg:-left-6 xl:left-5 2xl:left-35 z-10 text-[12px] font-semibold rounded-xl px-3 py-1 bg-gradient-to-r from-neutral-800/80  to-zinc-900/80 text-zinc-100   lg:hover:brightness-110"
           >
+            <p>+2 Years of Experience</p>
+          </div>
+          <div className="flex items-end justify-center rounded-full overflow-hidden w-98 h-98 lg:w-[400px] lg:h-[400px] backdrop-blur-md bg-neutral-700  border-10 border-neutral-950/80 shadow-2xl shadow-zinc-800/80 dark:shadow-cyan-500/15 lg:shadow-none lg:hover:shadow-2xl lg:hover:transform lg:hover:-translate-y-2  dark:lg:hover:shadow-cyan-500/10 lg:hover:shadow-zinc-800/80 transition-all duration-200 ease-in">
             <img
-              className=" w-4 brightness-0 dark:brightness-100 dark:lg:group-hover/linkedin:brightness-0 lg:group-hover/linkedin:brightness-100 "
-              src="./img/icons/linkedin-white.svg"
-              alt="linkedin icono"
+              className="w-full h-full object-top object-cover"
+              src="./img/photos/caricartura.png"
+              alt="foto de perfil"
             />
-          </a>
-          <a
-            href="https://www.instagram.com/miguelticaray/"
-            target="_blank"
-            className="p-3 dark:bg-teal-500/30 bg-teal-500/60  dark:lg:bg-transparent lg:bg-transparent  lg:hover:bg-teal-500 rounded-lg lg:hover:scale-110 transition-all duration-200 ease-in cursor-pointer group/instagram"
-          >
-            <img
-              className=" w-4 brightness-0 dark:brightness-100  dark:lg:group-hover/instagram:brightness-0 lg:group-hover/instagram:brightness-100 "
-              src="./img/icons/instagram-white.svg"
-              alt="instagram icono"
-            />
-          </a>
-        </div>
+          </div>
+        </section>
       </section>
-      <section className="flex justify-center 2xl:justify-end  items-center lg:w-full relative transition-all ease-in duration-200">
-        <div
-          ref={openToWork}
-          className="absolute -top-2 lg:top-5 -right-5 lg:right-2 xl:right-10 2xl:-right-15 z-10 text-[12px] text-zinc-900 font-semibold rounded-xl px-3 py-1 bg-gradient-to-r from-cyan-500 form-80% to-teal-400 lg:hover:brightness-110  "
-        >
-          <p>Open to Work</p>
-        </div>
-        <div
-          ref={experience}
-          className="absolute -bottom-5 lg:bottom-2  -left-5 lg:-left-6 xl:left-5 2xl:left-35 z-10 text-[12px] font-semibold rounded-xl px-3 py-1 bg-gradient-to-r from-neutral-800/80  to-zinc-900/80 text-zinc-100   lg:hover:brightness-110"
-        >
-          <p>+2 Years of Experience</p>
-        </div>
-        <div className="flex items-end justify-center rounded-full overflow-hidden w-98 h-98 lg:w-[400px] lg:h-[400px] backdrop-blur-md bg-neutral-700  border-10 border-neutral-950/80 shadow-2xl shadow-zinc-800/80 dark:shadow-cyan-500/15 lg:shadow-none lg:hover:shadow-2xl lg:hover:transform lg:hover:-translate-y-2  dark:lg:hover:shadow-cyan-500/10 lg:hover:shadow-zinc-800/80 transition-all duration-200 ease-in">
-          <img
-            className="w-full h-full object-top object-cover"
-            src="./img/photos/caricartura.png"
-            alt="foto de perfil"
-          />
-        </div>
-      </section>
-    </section>
+    </div>
   );
 }
 
